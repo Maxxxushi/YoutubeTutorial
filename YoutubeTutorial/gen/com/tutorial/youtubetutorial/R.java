d09279265f4825d801710d9004398d70b0ab43c9
maxxxushi <maxxxushi@gmail.com>
2013-01-15 13:27:09 -0500

    finished tut 10

--- YoutubeTutorial/gen/com/tutorial/youtubetutorial/R.java
+++ YoutubeTutorial/gen/com/tutorial/youtubetutorial/R.java
@@ -14,7 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int bAdd=0x7f070001;
+        public static final int bSub=0x7f070002;
+        public static final int menu_settings=0x7f070003;
+        public static final int tvDisplay=0x7f070000;
     }
     public static final class layout {
         public static final int activity_starting_point=0x7f030000;
@@ -24,8 +27,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int bAddOne=0x7f040002;
+        public static final int bSubOne=0x7f040003;
+        public static final int menu_settings=0x7f040001;
+        public static final int total=0x7f040004;
     }
     public static final class style {
         /** 
